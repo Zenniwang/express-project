96cf613682cb345820c4880cabdc60b6ff216433
zhen.karilainen <zhen.karilainen@edu.turkuamk.fi>
2018-08-28 22:56:28 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const hbs = require('hbs');
+
+//PORT run with Heroku or run locally
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+//middleware example
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    console.log(now);
+    next();
+})
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+
+
+app.get('/',(req,res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMsg: 'Welcome home'
+    });
+});
+
+app.get('/about',(req,res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is set up on port ${port}`);
+});
